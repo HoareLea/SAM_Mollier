eb3d7ba98a6012e9adba6c431ed98787df7e88bc
Ziolkowski <jakubziolkowski@digiproject.uk>
2022-06-23 15:10:06 +0100

    DryBulbTemperature Grasshopper Component Added

---
+++ Grasshopper/SAM.Core.Grasshopper.Mollier/Component/SAMMollierDryBulbTemperature.cs
@@ -0,0 +1,112 @@
+﻿using Grasshopper.Kernel;
+using SAM.Core.Grasshopper.Mollier.Properties;
+using SAM.Core.Grasshopper;
+using System;
+using System.Collections.Generic;
+
+namespace SAM.Analytical.Grasshopper
+{
+    public class SAMMollierDryBulbTemperature : GH_SAMVariableOutputParameterComponent
+    {
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid => new Guid("89d822cf-698b-4563-941c-514b5fc3e5d0");
+
+        /// <summary>
+        /// The latest version of this component
+        /// </summary>
+        public override string LatestComponentVersion => "1.0.0";
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon => Resources.SAM_Small;
+
+        public override GH_Exposure Exposure => GH_Exposure.primary;
+
+        protected override GH_SAMParam[] Inputs
+        {
+            get
+            {
+                List<GH_SAMParam> result = new List<GH_SAMParam>();
+                result.Add(new GH_SAMParam(new global::Grasshopper.Kernel.Parameters.Param_Number() { Name = "_density", NickName = "_density", Description = "Density [kg/m3]", Access = GH_ParamAccess.item }, ParamVisibility.Binding));
+                result.Add(new GH_SAMParam(new global::Grasshopper.Kernel.Parameters.Param_Number() { Name = "_relativeHumidity", NickName = "_relativeHumidity", Description = "Relative humidity (0 - 100) [%]", Access = GH_ParamAccess.item, Optional = true }, ParamVisibility.Binding));
+
+                global::Grasshopper.Kernel.Parameters.Param_Number param_Number = new global::Grasshopper.Kernel.Parameters.Param_Number() { Name = "_pressure_", NickName = "_pressure_", Description = "Atmospheric pressure [Pa]", Access = GH_ParamAccess.item, Optional = true };
+                param_Number.SetPersistentData(101325);
+                result.Add(new GH_SAMParam(param_Number, ParamVisibility.Voluntary));
+
+                return result.ToArray();
+            }
+        }
+
+        protected override GH_SAMParam[] Outputs
+        {
+            get
+            {
+                List<GH_SAMParam> result = new List<GH_SAMParam>();
+                result.Add(new GH_SAMParam(new global::Grasshopper.Kernel.Parameters.Param_Number() { Name = "dryBulbTemperature", NickName = "dryBulbTemperature", Description = "Dry bulb temperature [°C]", Access = GH_ParamAccess.item }, ParamVisibility.Binding));
+
+                return result.ToArray();
+            }
+        }
+
+        /// <summary>
+        /// Updates PanelTypes for AdjacencyCluster
+        /// </summary>
+        public SAMMollierDryBulbTemperature()
+          : base("SAMMollier.DryBulbTemperature", "SAMMollier.DryBulbTemperature",
+              "Utility function to calculate Dry Bulb Temperature",
+              "SAM", "Mollier")
+        {
+        }
+
+        protected override void SolveInstance(IGH_DataAccess dataAccess)
+        {
+            int index;
+
+            index = Params.IndexOfInputParam("_density");
+            if (index == -1)
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
+                return;
+            }
+            double density = double.NaN;
+            if (!dataAccess.GetData(index, ref density) || double.IsNaN(density))
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
+                return;
+            }
+
+            index = Params.IndexOfInputParam("_relativeHumidity");
+            if (index == -1)
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
+                return;
+            }
+            double relativeHumidity = double.NaN;
+            if (!dataAccess.GetData(index, ref relativeHumidity) || double.IsNaN(relativeHumidity))
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
+                return;
+            }
+
+            double pressure = double.NaN;
+            index = Params.IndexOfInputParam("_pressure_");
+            if (index == -1 || !dataAccess.GetData(index, ref pressure) || double.IsNaN(pressure))
+            {
+                pressure = 101325;
+            }
+
+            double dryBulbTemperature = Core.Mollier.Query.DryBulbTemperature_ByDensityAndRelativeHumidity(density, relativeHumidity, pressure);
+
+
+            index = Params.IndexOfOutputParam("dryBulbTemperature");
+            if (index != -1)
+            {
+                dataAccess.SetData(index, dryBulbTemperature);
+            }
+        }
+    }
+}
