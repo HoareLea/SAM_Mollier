98e56525e9f71c3590c44ad3c7e5a65cecb88ca1
Ziolkowski <jakubziolkowski@digiproject.uk>
2022-06-24 13:37:03 +0100

    Calculate Method Implemented

--- SAM_Mollier/SAM.Core.Mollier/Query/DryBulbTemperature.cs
+++ SAM_Mollier/SAM.Core.Mollier/Query/DryBulbTemperature.cs
@@ -12,30 +12,38 @@ namespace SAM.Core.Mollier
         /// <returns>Dry-bulb temperature [°C]</returns>
         public static double DryBulbTemperature(double enthalpy, double humidityRatio)
         {
-            if(double.IsNaN(enthalpy) || double.IsNaN(humidityRatio))
+            if (double.IsNaN(enthalpy) || double.IsNaN(humidityRatio))
             {
                 return double.NaN;
             }
 
-            double result = -25;
-            double enthalpy_Temp = double.NaN;
-            do
-            {
-                result += 1;
-                enthalpy_Temp = 1.01 * result + humidityRatio * (2501 + 1.86 * result);
-            }
-            while (!double.IsNaN(enthalpy_Temp) && enthalpy_Temp <= enthalpy && result <= 120);
+            return Core.Query.Calculate((double x) => Enthalpy(x, humidityRatio), enthalpy, -50, 99.999);
 
-            int i = 0;
-            do
-            {
-                result -= 0.001;
-                enthalpy_Temp = 1.01 * result + humidityRatio * (2501 + 1.86 * result);
-                i++;
-            }
-            while (!double.IsNaN(enthalpy_Temp) && enthalpy_Temp > enthalpy && i <= 10000);
 
-            return result;
+            //if (double.IsNaN(enthalpy) || double.IsNaN(humidityRatio))
+            //{
+            //    return double.NaN;
+            //}
+
+            //double result = -25;
+            //double enthalpy_Temp = double.NaN;
+            //do
+            //{
+            //    result += 1;
+            //    enthalpy_Temp = 1.01 * result + humidityRatio * (2501 + 1.86 * result);
+            //}
+            //while (!double.IsNaN(enthalpy_Temp) && enthalpy_Temp <= enthalpy && result <= 120);
+
+            //int i = 0;
+            //do
+            //{
+            //    result -= 0.001;
+            //    enthalpy_Temp = 1.01 * result + humidityRatio * (2501 + 1.86 * result);
+            //    i++;
+            //}
+            //while (!double.IsNaN(enthalpy_Temp) && enthalpy_Temp > enthalpy && i <= 10000);
+
+            //return result;
             //return ((enthalpy / 1000) - 2501 * humidityRatio) / (1.005 + humidityRatio * 1.86);
         }
 
@@ -53,26 +61,28 @@ namespace SAM.Core.Mollier
                 return double.NaN;
             }
 
-            double result = 50;
-            double enthalpy_Temp = double.NaN;
-            do
-            {
-                result -= 0.1;
-                enthalpy_Temp = Enthalpy_ByRelativeHumidity(result, relativeHumidity, pressure);
-            } while (!double.IsNaN(enthalpy_Temp) && enthalpy_Temp > enthalpy && result > -20);
+            return Core.Query.Calculate((double x) => Enthalpy_ByRelativeHumidity(x, relativeHumidity, pressure), enthalpy, -50, 99.999);
 
-            do
-            {
-                result += 0.0005;
-                enthalpy_Temp = Enthalpy_ByRelativeHumidity(result, relativeHumidity, pressure);
-            } while (!double.IsNaN(enthalpy_Temp) && enthalpy_Temp <= enthalpy && result <= 50);
+            //double result = 50;
+            //double enthalpy_Temp = double.NaN;
+            //do
+            //{
+            //    result -= 0.1;
+            //    enthalpy_Temp = Enthalpy_ByRelativeHumidity(result, relativeHumidity, pressure);
+            //} while (!double.IsNaN(enthalpy_Temp) && enthalpy_Temp > enthalpy && result > -20);
 
-            if (result > 100)
-            {
-                return double.NaN;
-            }
+            //do
+            //{
+            //    result += 0.0005;
+            //    enthalpy_Temp = Enthalpy_ByRelativeHumidity(result, relativeHumidity, pressure);
+            //} while (!double.IsNaN(enthalpy_Temp) && enthalpy_Temp <= enthalpy && result <= 50);
 
-            return result;
+            //if (result > 100)
+            //{
+            //    return double.NaN;
+            //}
+
+            //return result;
         }
 
         /// <summary>
@@ -84,26 +94,34 @@ namespace SAM.Core.Mollier
         /// <returns>Dry-bulb temperature [°C]</returns>
         public static double DryBulbTemperature_ByHumidityRatio(double humidityRatio, double relativeHumidity, double pressure)
         {
-            double result = 50;
-            double humidityRatio_Temp = double.NaN;
-            do
-            {
-                result -= 0.1;
-                humidityRatio_Temp = HumidityRatio(result, relativeHumidity, pressure);
-            } while (!double.IsNaN(humidityRatio_Temp) && humidityRatio_Temp > humidityRatio && result > -20);
-
-            do
-            {
-                result += 0.0005;
-                humidityRatio_Temp = HumidityRatio(result, relativeHumidity, pressure);
-            } while (!double.IsNaN(humidityRatio_Temp) && humidityRatio_Temp <= humidityRatio && result <= 50);
-
-            if (result > 100)
+            if (double.IsNaN(humidityRatio) || double.IsNaN(relativeHumidity) || double.IsNaN(pressure))
             {
                 return double.NaN;
             }
 
-            return result;
+
+            return Core.Query.Calculate((double x) => HumidityRatio(x, relativeHumidity, pressure), humidityRatio, -50, 99.999);
+
+            //double result = 50;
+            //double humidityRatio_Temp = double.NaN;
+            //do
+            //{
+            //    result -= 0.1;
+            //    humidityRatio_Temp = HumidityRatio(result, relativeHumidity, pressure);
+            //} while (!double.IsNaN(humidityRatio_Temp) && humidityRatio_Temp > humidityRatio && result > -20);
+
+            //do
+            //{
+            //    result += 0.0005;
+            //    humidityRatio_Temp = HumidityRatio(result, relativeHumidity, pressure);
+            //} while (!double.IsNaN(humidityRatio_Temp) && humidityRatio_Temp <= humidityRatio && result <= 50);
+
+            //if (result > 100)
+            //{
+            //    return double.NaN;
+            //}
+
+            //return result;
         }
 
         /// <summary>
@@ -115,26 +133,34 @@ namespace SAM.Core.Mollier
         /// <returns>Dry-bulb temperature [°C]</returns>
         public static double DryBulbTemperature_ByDensityAndHumidityRatio(double density, double humidityRatio, double pressure)
         {
-            double result = 110;
-            double density_Temp = double.NaN;
-            do
-            {
-                result -= 10;
-                density_Temp = Density_ByHumidityRatio(result, humidityRatio, pressure);
-            } while (!double.IsNaN(density_Temp) && density_Temp > density && result > -20);
-
-            do
-            {
-                result += 0.005;
-                density_Temp = Density_ByHumidityRatio(result, humidityRatio, pressure);
-            } while (!double.IsNaN(density_Temp) && density_Temp <= density && result <= 110);
-
-            if (result > 100)
+            if (double.IsNaN(humidityRatio) || double.IsNaN(density) || double.IsNaN(pressure))
             {
                 return double.NaN;
             }
 
-            return result;
+
+            return Core.Query.Calculate((double x) => Density_ByHumidityRatio(x, humidityRatio, pressure), density, -50, 99.999);
+
+            //double result = 110;
+            //double density_Temp = double.NaN;
+            //do
+            //{
+            //    result -= 10;
+            //    density_Temp = Density_ByHumidityRatio(result, humidityRatio, pressure);
+            //} while (!double.IsNaN(density_Temp) && density_Temp > density && result > -20);
+
+            //do
+            //{
+            //    result += 0.005;
+            //    density_Temp = Density_ByHumidityRatio(result, humidityRatio, pressure);
+            //} while (!double.IsNaN(density_Temp) && density_Temp <= density && result <= 110);
+
+            //if (result > 100)
+            //{
+            //    return double.NaN;
+            //}
+
+            //return result;
         }
 
         /// <summary>
@@ -146,26 +172,34 @@ namespace SAM.Core.Mollier
         /// <returns>Dry-bulb temperature [°C]</returns>
         public static double DryBulbTemperature_ByDensityAndRelativeHumidity(double density, double relativeHumidity, double pressure)
         {
-            double result = 100;
-            double density_Temp = double.NaN;
-            do
-            {
-                result -= 10;
-                density_Temp = Density(result, relativeHumidity, pressure);
-            } while (!double.IsNaN(density_Temp) && density_Temp <= density && result > -20);
-
-            do
-            {
-                result += 0.005;
-                density_Temp = Density(result, relativeHumidity, pressure);
-            } while (!double.IsNaN(density_Temp) && density_Temp > density && result <= 100);
-
-            if (result > 100)
+            if (double.IsNaN(relativeHumidity) || double.IsNaN(density) || double.IsNaN(pressure))
             {
                 return double.NaN;
             }
 
-            return result;
+
+            return Core.Query.Calculate((double x) => Density(x, relativeHumidity, pressure), density, -50, 99.999);
+
+            //double result = 100;
+            //double density_Temp = double.NaN;
+            //do
+            //{
+            //    result -= 10;
+            //    density_Temp = Density(result, relativeHumidity, pressure);
+            //} while (!double.IsNaN(density_Temp) && density_Temp <= density && result > -20);
+
+            //do
+            //{
+            //    result += 0.005;
+            //    density_Temp = Density(result, relativeHumidity, pressure);
+            //} while (!double.IsNaN(density_Temp) && density_Temp > density && result <= 100);
+
+            //if (result > 100)
+            //{
+            //    return double.NaN;
+            //}
+
+            //return result;
         }
     }
 }
