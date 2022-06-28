171ae4086fa0f84590384199ab87b80ebca43570
Ziolkowski <jakubziolkowski@digiproject.uk>
2022-06-28 15:04:34 +0100

    DiagramTemperature Updated

--- SAM_Mollier/SAM.Core.Mollier/Query/DiagramTemperature.cs
+++ SAM_Mollier/SAM.Core.Mollier/Query/DiagramTemperature.cs
@@ -1,23 +1,22 @@
-﻿using System;
-
+﻿
 namespace SAM.Core.Mollier
 {
     public static partial class Query
     {
         /// <summary>
-        /// Calculates humidity ratio from dry bulb temperature, relative humidity and pressure.
+        /// Calculates humidity ratio from dry bulb temperature, humidity ratio and pressure.
         /// </summary>
         /// <param name="dryBulbTemperature">Dry bulb temperature [°C]</param>
-        /// <param name="relativeHumidity">Relative humidity (0 - 100)[%]</param>
+        /// <param name="humidityRatio">Humidity Ratio [kg_waterVapor/kg_dryAir]</param>
         /// <returns>Diagram Temperature [°C]</returns>
-        public static double DiagramTemperature(double dryBulbTemperature, double relativeHumidity)
+        public static double DiagramTemperature(double dryBulbTemperature, double humidityRatio)
         {
-            if (double.IsNaN(relativeHumidity) || relativeHumidity > 100 || relativeHumidity < 0 || double.IsNaN(dryBulbTemperature))
+            if (double.IsNaN(humidityRatio) || double.IsNaN(dryBulbTemperature))
             {
                 return double.NaN;
             }
 
-            return dryBulbTemperature + relativeHumidity / 1000 * 1.86 * dryBulbTemperature;
+            return dryBulbTemperature + humidityRatio * 1.86 * dryBulbTemperature;
         }
     }
 }
