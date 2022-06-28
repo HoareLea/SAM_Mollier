37f8e0350d5dc481630de36cc2b57373c05c749b
Ziolkowski <jakubziolkowski@digiproject.uk>
2022-06-28 10:26:17 +0100

    Missing File Updated

---
+++ SAM_Mollier/SAM.Core.Mollier/Query/DynamicViscosity.cs
@@ -0,0 +1,21 @@
+﻿namespace SAM.Core.Mollier
+{
+    public static partial class Query
+    {
+        /// <summary>
+        /// Calculates specific volume from dry bulb temperature, relative humidity and pressure.
+        /// </summary>
+        /// <param name="dryBulbTemperature">Dry bulb temperature [°C]</param>
+        /// <param name="humidityRatio">Humidity Ratio [kg_waterVapor/kg_dryAir]</param>
+        /// <returns>Specific Volume [m3/kg]</returns>
+        public static double DynamicViscosity(double dryBulbTemperature, double humidityRatio)
+        {
+            if(double.IsNaN(dryBulbTemperature) || double.IsNaN(humidityRatio) || double.IsNaN(pressure))
+            {
+                return double.NaN;
+            }
+
+            return (humidityRatio + 0.622) * 461.53 * ((273.15 + dryBulbTemperature) / pressure);
+        }
+    }
+}
