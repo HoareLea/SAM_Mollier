41f9b2937e6904b277298bb4c7299be9903b70d6
michaldengusiak <tassimulation@gmail.com>
2022-06-30 16:45:31 +0100

    Update SAMMollierCalculateAHU.cs

--- Grasshopper/SAM.Analytical.Grasshopper.Mollier/Component/SAMMollierCalculateAHU.cs
+++ Grasshopper/SAM.Analytical.Grasshopper.Mollier/Component/SAMMollierCalculateAHU.cs
@@ -45,7 +45,7 @@ namespace SAM.Analytical.Grasshopper
                 List<GH_SAMParam> result = new List<GH_SAMParam>();
                 result.Add(new GH_SAMParam(new GooAnalyticalModelParam() { Name = "analyticalModel", NickName = "analyticalModel", Description = "SAM Analytical Model", Access = GH_ParamAccess.item }, ParamVisibility.Binding));
                 result.Add(new GH_SAMParam(new global::Grasshopper.Kernel.Parameters.Param_Number() { Name = "sensibleHeatLoss", NickName = "sensibleHeatLoss", Description = "Sensible Heat Loss for connected Spaces [kW]", Access = GH_ParamAccess.item }, ParamVisibility.Binding));
-                result.Add(new GH_SAMParam(new global::Grasshopper.Kernel.Parameters.Param_Number() { Name = "sensibleHeatGains", NickName = "sensibleHeatGains", Description = "Sensible Heat Gains for connected Spaces [kW]", Access = GH_ParamAccess.item }, ParamVisibility.Binding));
+                result.Add(new GH_SAMParam(new global::Grasshopper.Kernel.Parameters.Param_Number() { Name = "sensibleHeatGain", NickName = "sensibleHeatGain", Description = "Sensible Heat Gain for connected Spaces [kW]", Access = GH_ParamAccess.item }, ParamVisibility.Binding));
                 return result.ToArray();
             }
         }
