cbcbbe20b4ab2aae35ee8aca5f093a5848b613ef
Ziolkowski <jakubziolkowski@digiproject.uk>
2022-06-30 16:29:10 +0100

    Resources Updated

--- Grasshopper/SAM.Analytical.Grasshopper.Mollier/Properties/Resources.Designer.cs
+++ Grasshopper/SAM.Analytical.Grasshopper.Mollier/Properties/Resources.Designer.cs
@@ -39,7 +39,7 @@ namespace SAM.Analytical.Grasshopper.Mollier.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("SAM.Core.Grasshopper.Mollier.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("SAM.Analytical.Grasshopper.Mollier.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
@@ -73,59 +73,9 @@ namespace SAM.Analytical.Grasshopper.Mollier.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap SAM_Explode {
-            get {
-                object obj = ResourceManager.GetObject("SAM_Explode", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap SAM_Get {
-            get {
-                object obj = ResourceManager.GetObject("SAM_Get", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap SAM_Get_Filterpng {
-            get {
-                object obj = ResourceManager.GetObject("SAM_Get_Filterpng", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap SAM_JSON {
-            get {
-                object obj = ResourceManager.GetObject("SAM_JSON", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
         internal static System.Drawing.Bitmap SAM_Mollier {
             get {
                 object obj = ResourceManager.GetObject("SAM_Mollier", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap SAM_Names {
-            get {
-                object obj = ResourceManager.GetObject("SAM_Names", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
