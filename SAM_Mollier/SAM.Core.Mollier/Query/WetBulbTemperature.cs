2735525829bb7d748238026b6f3a7b65c49d05b5
michaldengusiak <tassimulation@gmail.com>
2022-07-01 12:22:22 +0100

    Update WetBulbTemperature.cs

--- SAM_Mollier/SAM.Core.Mollier/Query/WetBulbTemperature.cs
+++ SAM_Mollier/SAM.Core.Mollier/Query/WetBulbTemperature.cs
@@ -15,33 +15,28 @@
                 return double.NaN;
             }
 
-            return Core.Query.Calculate((double x) => Enthalpy_ByRelativeHumidity(x, 100, pressure), enthalpy, 0.01, 99.99);
+            //return Core.Query.Calculate((double x) => Enthalpy_ByRelativeHumidity(x, 100, pressure), enthalpy, 0.01, 99.99);
 
 
             //double enthalpy = Enthalpy_ByRelativeHumidity(dryBulbTemperature, relativeHumidity, pressure);
 
-            //double result = -4;
-            //double enthaply_Temp = double.NaN;
-            //do
-            //{
-            //    result += 5;
-            //    enthaply_Temp = Enthalpy_ByRelativeHumidity(dryBulbTemperature, 100, pressure);
+            double result = -4;
+            double enthaply_Temp = double.NaN;
+            do
+            {
+                result += 5;
+                enthaply_Temp = Enthalpy_ByRelativeHumidity(result, 100, pressure);
 
-            //} while (!double.IsNaN(enthaply_Temp) && enthaply_Temp <= enthalpy);
+            } while (!double.IsNaN(enthaply_Temp) && enthaply_Temp <= enthalpy);
 
-            //do
-            //{
-            //    result -= 0.01;
-            //    enthaply_Temp = Enthalpy_ByRelativeHumidity(dryBulbTemperature, 100, pressure);
+            do
+            {
+                result -= 0.01;
+                enthaply_Temp = Enthalpy_ByRelativeHumidity(result, 100, pressure);
 
-            //} while (!double.IsNaN(enthaply_Temp) && enthaply_Temp > enthalpy);
+            } while (!double.IsNaN(enthaply_Temp) && enthaply_Temp > enthalpy);
 
-            //if(result < 0)
-            //{
-            //    return double.NaN;
-            //}
-
-            //return result;
+            return result;
         }
     }
 }
