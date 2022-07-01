06eca60bd1986b2cf79bc8783facc947c178ed31
Ziolkowski <jakubziolkowski@digiproject.uk>
2022-07-01 09:40:15 +0100

    Pressure Query for Elevation Added

---
+++ SAM_Mollier/SAM.Core.Mollier/Query/Pressure.cs
@@ -0,0 +1,15 @@
+﻿namespace SAM.Core.Mollier
+{
+    public static partial class Query
+    {
+        /// <summary>
+        /// Calculates air pressure from elevation
+        /// </summary>
+        /// <param name="elevation">Elevation [m]</param>
+        /// <returns>Pressure [Pa]</returns>
+        public static double Pressure(double elevation)
+        {
+            return 101325 * System.Math.Pow((1 - 0.0000225577 * elevation), 5.2559);
+        }
+    }
+}
