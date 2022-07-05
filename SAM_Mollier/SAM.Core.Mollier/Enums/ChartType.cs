9e740eb34a53366d3a470543c13eda4a606aa2ed
Maciej Dengusiak <maciejdengusiak@gmail.com>
2022-07-05 16:10:57 +0200

    Chart type enum added

---
+++ SAM_Mollier/SAM.Core.Mollier/Enums/ChartType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAM.Core.Mollier
+{
+    public enum ChartType
+    {
+        [Description("Undefined")] Undefined,
+        [Description("Mollier")] Mollier,
+        [Description("Psychrometric")] Psychrometric
+    }
+}
