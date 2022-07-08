f4b43f8dc8c70a98a3f128cc30923edfb95e446f
michaldengusiak <tassimulation@gmail.com>
2022-07-08 12:45:55 +0100

    Update HeatRecoveryProcess.cs

--- SAM_Mollier/SAM.Core.Mollier/Create/HeatRecoveryProcess.cs
+++ SAM_Mollier/SAM.Core.Mollier/Create/HeatRecoveryProcess.cs
@@ -2,6 +2,15 @@
 {
     public static partial class Create
     {
+
+        /// <summary>
+        /// Creates HeatRecoveryProcess based on supply and return air parameters and heat recovery efficiences
+        /// </summary>
+        /// <param name="supply">Supply air parameters</param>
+        /// <param name="return">Return air parameters</param>
+        /// <param name="sensibleHeatRecoveryEfficiency">Sensible Heat Recovery Efficiency [%]</param>
+        /// <param name="latentHeatRecoveryEfficiency">Latent Heat Recovery Efficiency [%]</param>
+        /// <returns>HeatRecoveryProcess</returns>
         public static HeatRecoveryProcess HeatRecoveryProcess(this MollierPoint supply, MollierPoint @return, double sensibleHeatRecoveryEfficiency, double latentHeatRecoveryEfficiency)
         {
             if (supply == null || @return == null || double.IsNaN(sensibleHeatRecoveryEfficiency) || double.IsNaN(latentHeatRecoveryEfficiency))
@@ -9,13 +18,13 @@
                 return null;
             }
 
-            double dryBulbTemperature = supply.DryBulbTemperature + ((@return.DryBulbTemperature - supply.DryBulbTemperature) * sensibleHeatRecoveryEfficiency);
+            double dryBulbTemperature = supply.DryBulbTemperature + ((@return.DryBulbTemperature - supply.DryBulbTemperature) * sensibleHeatRecoveryEfficiency /100);
             if(double.IsNaN(dryBulbTemperature))
             {
                 return null;
             }
 
-            double humidityRatio = supply.HumidityRatio + ((@return.HumidityRatio - supply.HumidityRatio) * latentHeatRecoveryEfficiency);
+            double humidityRatio = supply.HumidityRatio + ((@return.HumidityRatio - supply.HumidityRatio) * latentHeatRecoveryEfficiency / 100);
             if (double.IsNaN(humidityRatio))
             {
                 return null;
