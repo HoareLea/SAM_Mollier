df83583e1c4ce3bbd5c988dc07e247ef3f64744b
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2023-06-13 14:11:09 +0200

    UIMollierAppearance Implemented

---
+++ SAM_Mollier/SAM.Core.Mollier/Classes/UIMollierAppearance.cs
@@ -0,0 +1,78 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Drawing;
+
+namespace SAM.Core.Mollier
+{
+    public class UIMollierAppearance : IJSAMObject
+    {
+        public Color Color { get; set; }
+        
+        public string Label { get; set; }
+
+        public UIMollierAppearance(Color color, string label)
+        {
+            Color = color;
+            Label = label;
+        }
+
+        public UIMollierAppearance(UIMollierAppearance uIMollierAppearance)
+        {
+            if(uIMollierAppearance != null)
+            {
+                Color = uIMollierAppearance.Color;
+                Label = uIMollierAppearance.Label;
+            }
+        }
+
+        public UIMollierAppearance(JObject jObject)
+        {
+            FromJObject(jObject);
+        }
+
+        public bool FromJObject(JObject jObject)
+        {
+            if (jObject == null)
+            {
+                return false;
+            }
+            
+            if (jObject.ContainsKey("Color"))
+            {
+                JObject jObject_Color = jObject.Value<JObject>("Color");
+                if (jObject_Color != null)
+                {
+                    SAMColor sAMColor = new SAMColor(jObject_Color);
+                    if (sAMColor != null)
+                    {
+                        Color = sAMColor.ToColor();
+                    }
+                }
+            }
+
+            if (jObject.ContainsKey("Label"))
+            {
+                Label = jObject.Value<string>("Label");
+            }
+
+            return true;
+        }
+        
+        public JObject ToJObject()
+        {
+            JObject jObject = new JObject();
+            jObject.Add("_type", Core.Query.FullTypeName(this));
+
+            if (Color != Color.Empty)
+            {
+                jObject.Add("Color", (new SAMColor(Color)).ToJObject());
+            }
+
+            if (Label != null)
+            {
+                jObject.Add("Label", Label);
+            }
+
+            return jObject;
+        }
+    }
+}
---
+++ SAM_Mollier/SAM.Core.Mollier/Classes/UIMollierPoint.cs
@@ -0,0 +1,101 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Drawing;
+
+namespace SAM.Core.Mollier
+{
+    public class UIMollierPoint : IMollierPoint
+    {
+        private MollierPoint mollierPoint;
+
+        private UIMollierAppearance uIMollierAppearance;
+        
+        public double Pressure
+        {
+            get
+            {
+
+                return mollierPoint == null ? double.NaN : mollierPoint.Pressure;
+            }
+        }
+
+        public MollierPoint MollierPoint
+        {
+            get
+            {
+                return mollierPoint?.Clone();
+            }
+        }
+
+        public UIMollierAppearance UIMollierAppearance
+        {
+            get
+            {
+                return uIMollierAppearance;
+            }
+
+            set
+            {
+                uIMollierAppearance = value;
+            }
+        }
+
+        public UIMollierPoint(MollierPoint mollierPoint, Color color, string label)
+        {
+            this.mollierPoint = mollierPoint?.Clone();
+            uIMollierAppearance = new UIMollierAppearance(color, label);
+        }
+
+        public UIMollierPoint(UIMollierPoint uIMollierPoint)
+        {
+            if(uIMollierPoint != null)
+            {
+                mollierPoint = uIMollierPoint.mollierPoint?.Clone();
+                uIMollierAppearance = uIMollierPoint.uIMollierAppearance?.Clone();
+            }
+        }
+
+        public UIMollierPoint(JObject jObject)
+        {
+            FromJObject(jObject);
+        }
+
+        public bool FromJObject(JObject jObject)
+        {
+            if (jObject == null)
+            {
+                return false;
+            }
+
+            if (jObject.ContainsKey("MollierPoint"))
+            {
+                mollierPoint = new MollierPoint(jObject.Value<JObject>("MollierPoint"));
+            }
+
+            if (jObject.ContainsKey("UIMollierAppearance"))
+            {
+                uIMollierAppearance = new UIMollierAppearance(jObject.Value<JObject>("UIMollierAppearance"));
+            }
+
+            return true;
+        }
+        
+        public JObject ToJObject()
+        {
+            JObject jObject = new JObject();
+            jObject.Add("_type", Core.Query.FullTypeName(this));
+
+            if(mollierPoint != null)
+            {
+                jObject.Add("MollierPoint", mollierPoint.ToJObject());
+            }
+
+            if (uIMollierAppearance != null)
+            {
+                jObject.Add("UIMollierAppearance", uIMollierAppearance.ToJObject());
+            }
+
+
+            return jObject;
+        }
+    }
+}
