3860b2380348a926c0593d443d5c24b91dec96f8
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2023-06-14 17:22:08 +0200

    Efficiency Query Added

---
+++ SAM_Mollier/SAM.Core.Mollier/Query/Efficiency.cs
@@ -0,0 +1,29 @@
+﻿namespace SAM.Core.Mollier
+{
+    public static partial class Query
+    {
+        /// <summary>
+        /// Process Efficiency in percent [%]
+        /// </summary>
+        /// <param name="dryBulbTemperature_1">Start Dry Bulb Temperature</param>
+        /// <param name="dryBulbTemperature_2">End Dry Bulb Temperature</param>
+        /// <param name="supplyTemperature">Supply Temperature</param>
+        /// <param name="returnTemperature">Return Temperature</param>
+        /// <param name="tolerance">Rounding Tolerance</param>
+        /// <returns>Efficiency [%]</returns>
+        public static double Efficiency(double dryBulbTemperature_1, double dryBulbTemperature_2, double supplyTemperature, double returnTemperature, double tolerance = SAM.Core.Tolerance.MacroDistance)
+        {
+            if(double.IsNaN(dryBulbTemperature_1) || double.IsNaN(dryBulbTemperature_2) || double.IsNaN(supplyTemperature) || double.IsNaN(returnTemperature))
+            {
+                return double.NaN;
+            }
+
+            double meanTemperature = (supplyTemperature + returnTemperature) / 2;
+
+            double result = Core.Query.AlmostEqual(System.Math.Abs(dryBulbTemperature_1 - meanTemperature), 0) ? 0 : System.Math.Abs((dryBulbTemperature_1 - dryBulbTemperature_2) / (dryBulbTemperature_1 - meanTemperature));
+            result = Core.Query.Round(result, tolerance) * 100;
+
+            return result;
+        }
+    }
+}
