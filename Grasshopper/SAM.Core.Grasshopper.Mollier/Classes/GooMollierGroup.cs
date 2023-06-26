2da316c2f9d9b387df14dfc13e17b971b9440e9d
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2023-06-26 16:06:43 +0200

    GooMollierGroup and GooMollierGroupParam Switched Off

--- Grasshopper/SAM.Core.Grasshopper.Mollier/Classes/GooMollierGroup.cs
+++ Grasshopper/SAM.Core.Grasshopper.Mollier/Classes/GooMollierGroup.cs
@@ -1,65 +1,65 @@
-﻿using Grasshopper.Kernel;
-using Grasshopper.Kernel.Types;
-using SAM.Core.Grasshopper.Mollier.Properties;
-using SAM.Core.Mollier;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Windows.Forms;
+﻿//using Grasshopper.Kernel;
+//using Grasshopper.Kernel.Types;
+//using SAM.Core.Grasshopper.Mollier.Properties;
+//using SAM.Core.Mollier;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Windows.Forms;
 
-namespace SAM.Core.Grasshopper.Mollier
-{
-    public class GooMollierGroup : GooJSAMObject<IMollierGroup>
-    {
-        public GooMollierGroup()
-            : base()
-        {
-        }
+//namespace SAM.Core.Grasshopper.Mollier
+//{
+//    public class GooMollierGroup : GooJSAMObject<IMollierGroup>
+//    {
+//        public GooMollierGroup()
+//            : base()
+//        {
+//        }
 
-        public GooMollierGroup(IMollierGroup mollierGroup)
-            : base(mollierGroup)
-        {
-        }
+//        public GooMollierGroup(IMollierGroup mollierGroup)
+//            : base(mollierGroup)
+//        {
+//        }
 
-        public override IGH_Goo Duplicate()
-        {
-            return new GooMollierGroup(Value);
-        }
-    }
+//        public override IGH_Goo Duplicate()
+//        {
+//            return new GooMollierGroup(Value);
+//        }
+//    }
 
-    public class GooMollierGroupParam : GH_PersistentParam<GooMollierPoint>
-    {
-        public override Guid ComponentGuid => new Guid("2bc4d547-aa2b-473e-b42e-575722ce8367");
+//    public class GooMollierGroupParam : GH_PersistentParam<GooMollierPoint>
+//    {
+//        public override Guid ComponentGuid => new Guid("2bc4d547-aa2b-473e-b42e-575722ce8367");
 
-        protected override System.Drawing.Bitmap Icon => Resources.SAM_Small;
+//        protected override System.Drawing.Bitmap Icon => Resources.SAM_Small;
 
-        public GooMollierGroupParam()
-            : base("MollierGroup", "MollierGroup", "SAM Core Mollier MollierGroup", "Params", "SAM")
-        {
-        }
+//        public GooMollierGroupParam()
+//            : base("MollierGroup", "MollierGroup", "SAM Core Mollier MollierGroup", "Params", "SAM")
+//        {
+//        }
 
-        protected override GH_GetterResult Prompt_Plural(ref List<GooMollierPoint> values)
-        {
-            throw new NotImplementedException();
-        }
+//        protected override GH_GetterResult Prompt_Plural(ref List<GooMollierPoint> values)
+//        {
+//            throw new NotImplementedException();
+//        }
 
-        protected override GH_GetterResult Prompt_Singular(ref GooMollierPoint value)
-        {
-            throw new NotImplementedException();
-        }
+//        protected override GH_GetterResult Prompt_Singular(ref GooMollierPoint value)
+//        {
+//            throw new NotImplementedException();
+//        }
 
-        public override void AppendAdditionalMenuItems(ToolStripDropDown menu)
-        {
-            Menu_AppendItem(menu, "Save As...", Menu_SaveAs, VolatileData.AllData(true).Any());
+//        public override void AppendAdditionalMenuItems(ToolStripDropDown menu)
+//        {
+//            Menu_AppendItem(menu, "Save As...", Menu_SaveAs, VolatileData.AllData(true).Any());
 
-            //Menu_AppendSeparator(menu);
+//            //Menu_AppendSeparator(menu);
 
-            base.AppendAdditionalMenuItems(menu);
-        }
+//            base.AppendAdditionalMenuItems(menu);
+//        }
 
-        private void Menu_SaveAs(object sender, EventArgs e)
-        {
-            Grasshopper.Query.SaveAs(VolatileData);
-        }
-    }
-}
+//        private void Menu_SaveAs(object sender, EventArgs e)
+//        {
+//            Grasshopper.Query.SaveAs(VolatileData);
+//        }
+//    }
+//}
