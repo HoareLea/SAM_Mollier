9187b2d41df59a2de123e21a6b94379823e639e5
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2023-06-28 16:21:21 +0200

    Sensible Heat Ratio Query Added

--- SAM_Mollier/SAM.Core.Mollier/Query/SensibleHeatRatio.cs
+++ SAM_Mollier/SAM.Core.Mollier/Query/SensibleHeatRatio.cs
@@ -22,5 +22,24 @@
 
             return  sensibleGain / (sensibleGain + latentGain);
         }
+
+        /// <summary>
+        /// Calculates Sensible Heat Ratio [-]
+        /// </summary>
+        /// <param name="specificHeat">Specific Heat [kJ/kg*C][</param>
+        /// <param name="temperature_In">Temperature In [C]</param>
+        /// <param name="temperature_Out">Temperature Out [C]</param>
+        /// <param name="enthaply_In">Enthalpy In [kJ/kg]</param>
+        /// <param name="enthalpy_Out">Enthalpy Out [kJ/kg]</param>
+        /// <returns>Sensible Heat Ratio (SHR) [-]</returns>
+        public static double SensibleHeatRatio(double specificHeat, double temperature_In, double temperature_Out, double enthaply_In, double enthalpy_Out)
+        {
+            if(double.IsNaN(specificHeat) || double.IsNaN(temperature_In) || double.IsNaN(temperature_Out) || double.IsNaN(enthaply_In) || double.IsNaN(enthalpy_Out))
+            {
+                return double.NaN;
+            }
+
+            return specificHeat * (temperature_Out - temperature_In) / (enthalpy_Out - enthaply_In);
+        }
     }
 }
