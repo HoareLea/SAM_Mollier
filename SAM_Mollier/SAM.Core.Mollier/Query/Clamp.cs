bf8dc62a5c10638f18bf0634ddc0703ab0e2329c
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2023-07-26 10:36:51 +0200

    Limit Variables Added

--- SAM_Mollier/SAM.Core.Mollier/Query/Clamp.cs
+++ SAM_Mollier/SAM.Core.Mollier/Query/Clamp.cs
@@ -19,12 +19,6 @@ namespace SAM.Core.Mollier
                 return null;
             }
 
-            List<MollierPoint> mollierPoints_Range = Create.MollierPoints(humidityRatioRange, dryBulbTemperatureRange, constantValueCurve.Pressure);
-            if(mollierPoints_Range == null || mollierPoints_Range.Count == 0)
-            {
-                return null;
-            }
-            mollierPoints_Range.Add(mollierPoints_Range[0]);
 
             Func<MollierPoint, MollierPoint, double> func_Distance = new Func<MollierPoint, MollierPoint, double>((MollierPoint mollierPoint_1, MollierPoint mollierPoint_2) =>
             {
@@ -45,26 +39,100 @@ namespace SAM.Core.Mollier
                 MollierPoint start = mollierPoints[i];
                 MollierPoint end = mollierPoints[i + 1];
 
-                mollierPoints_Temp.Add(start);
+                List<Tuple<double, double>> tuples = new List<Tuple<double, double>>();
 
-                List<MollierPoint> mollierPoints_Intersection = Intersections(start, end, mollierPoints_Range, tolerance);
-                if(mollierPoints_Intersection == null || mollierPoints_Intersection.Count == 0)
+                double diagramTemperature_Start = start.DiagramTemperature();
+                double diagramTemperature_End = end.DiagramTemperature();
+
+                if (dryBulbTemperatureRange.In(diagramTemperature_Start) && humidityRatioRange.In(start.HumidityRatio))
+                {
+                    tuples.Add(new Tuple<double, double>(diagramTemperature_Start, start.HumidityRatio));
+                }
+
+                if (dryBulbTemperatureRange.In(diagramTemperature_End) && humidityRatioRange.In(end.HumidityRatio))
+                {
+                    tuples.Add(new Tuple<double, double>(diagramTemperature_End, end.HumidityRatio));
+                }
+
+                Intersection(start.HumidityRatio, diagramTemperature_Start, end.HumidityRatio, diagramTemperature_End, humidityRatioRange.Min, dryBulbTemperatureRange.Max, humidityRatioRange.Min + 1, dryBulbTemperatureRange.Max, out double dryBulbTemperature_1, out double humidityRatio_1);
+
+                Intersection(start.HumidityRatio, diagramTemperature_Start, end.HumidityRatio, diagramTemperature_End, humidityRatioRange.Min, dryBulbTemperatureRange.Min, humidityRatioRange.Min + 1, dryBulbTemperatureRange.Min, out double dryBulbTemperature_2, out double humidityRatio_2);
+
+                Intersection(start.HumidityRatio, diagramTemperature_Start, end.HumidityRatio, diagramTemperature_End, humidityRatioRange.Min, dryBulbTemperatureRange.Min, humidityRatioRange.Min, dryBulbTemperatureRange.Min + 1, out double dryBulbTemperature_3, out double humidityRatio_3);
+
+                Intersection(start.HumidityRatio, diagramTemperature_Start, end.HumidityRatio, diagramTemperature_End, humidityRatioRange.Max, dryBulbTemperatureRange.Min, humidityRatioRange.Max, dryBulbTemperatureRange.Min + 1, out double dryBulbTemperature_4, out double humidityRatio_4);
+
+                Range<double> dryBulbTemperatureRange_Temp = new Range<double>(diagramTemperature_Start, diagramTemperature_End);
+                Range<double> humidityRatioRange_Temp = new Range<double>(start.HumidityRatio, end.HumidityRatio);
+                
+
+                if(dryBulbTemperatureRange.In(dryBulbTemperature_1) && dryBulbTemperatureRange_Temp.In(dryBulbTemperature_1) && humidityRatioRange.In(humidityRatio_1) && humidityRatioRange_Temp.In(humidityRatio_1))
+                {
+                    tuples.Add(new Tuple<double, double>(dryBulbTemperature_1, humidityRatio_1));
+                }
+
+                if (dryBulbTemperatureRange.In(dryBulbTemperature_2) && dryBulbTemperatureRange_Temp.In(dryBulbTemperature_2) && humidityRatioRange.In(humidityRatio_2) && humidityRatioRange_Temp.In(humidityRatio_2))
+                {
+                    tuples.Add(new Tuple<double, double>(dryBulbTemperature_2, humidityRatio_2));
+                }
+
+                if (dryBulbTemperatureRange.In(dryBulbTemperature_3) && dryBulbTemperatureRange_Temp.In(dryBulbTemperature_3) && humidityRatioRange.In(humidityRatio_3) && humidityRatioRange_Temp.In(humidityRatio_3))
+                {
+                    tuples.Add(new Tuple<double, double>(dryBulbTemperature_3, humidityRatio_3));
+                }
+
+                if (dryBulbTemperatureRange.In(dryBulbTemperature_4) && dryBulbTemperatureRange_Temp.In(dryBulbTemperature_4) && humidityRatioRange.In(humidityRatio_4) && humidityRatioRange_Temp.In(humidityRatio_4))
+                {
+                    tuples.Add(new Tuple<double, double>(dryBulbTemperature_4, humidityRatio_4));
+                }
+
+                List<MollierPoint> mollierPoints_Tuples = new List<MollierPoint>();
+                foreach (Tuple<double, double> tuple in tuples)
+                {
+                    double dryBulbTemperature = DryBulbTemperature_ByDiagramTemperature(tuple.Item1, tuple.Item2, start.Pressure);
+                    double diagramTemperature = DiagramTemperature(dryBulbTemperature, tuple.Item2, start.Pressure);
+
+                    double difference = Math.Abs(tuple.Item1 - diagramTemperature);
+
+                    MollierPoint mollierPoint = new MollierPoint(dryBulbTemperature, tuple.Item2, start.Pressure);
+                    if(mollierPoint.IsValid())
+                    {
+                        mollierPoints_Tuples.Add(mollierPoint);
+                    }
+                }
+
+                if(mollierPoints_Tuples.Count == 0)
                 {
                     continue;
                 }
 
-
-                if (constantValueCurve.ChartDataType == ChartDataType.RelativeHumidity && constantValueCurve.Value == 100)
+                if(mollierPoints_Tuples.Count > 1)
                 {
-                    mollierPoints_Intersection = mollierPoints_Intersection.ConvertAll(x => new MollierPoint(x.DryBulbTemperature, HumidityRatio(x.DryBulbTemperature, 100, x.Pressure), x.Pressure));
+                    mollierPoints_Tuples.Sort((x, y) => func_Distance.Invoke(start, x).CompareTo(func_Distance.Invoke(start, y)));
                 }
 
-                if (mollierPoints_Intersection.Count > 1)
-                {
-                    mollierPoints_Intersection.Sort((x, y) => func_Distance.Invoke(start, x).CompareTo(func_Distance.Invoke(start, y)));
-                }
+                mollierPoints_Tuples.ForEach(x => mollierPoints_Temp.Add(x));
 
-                mollierPoints_Intersection.ForEach(x => mollierPoints_Temp.Add(x));
+                //mollierPoints_Temp.Add(start);
+
+                //List<MollierPoint> mollierPoints_Intersection = Intersections(start, end, mollierPoints_Range, tolerance);
+                //if(mollierPoints_Intersection == null || mollierPoints_Intersection.Count == 0)
+                //{
+                //    continue;
+                //}
+
+
+                //if (constantValueCurve.ChartDataType == ChartDataType.RelativeHumidity && constantValueCurve.Value == 100)
+                //{
+                //    mollierPoints_Intersection = mollierPoints_Intersection.ConvertAll(x => new MollierPoint(x.DryBulbTemperature, HumidityRatio(x.DryBulbTemperature, 100, x.Pressure), x.Pressure));
+                //}
+
+                //if (mollierPoints_Intersection.Count > 1)
+                //{
+                //    mollierPoints_Intersection.Sort((x, y) => func_Distance.Invoke(start, x).CompareTo(func_Distance.Invoke(start, y)));
+                //}
+
+                //mollierPoints_Intersection.ForEach(x => mollierPoints_Temp.Add(x));
             }
 
             mollierPoints_Temp.Add(mollierPoints.Last());
---
+++ SAM_Mollier/SAM.Core.Mollier/Variables/Limit.cs
@@ -0,0 +1,24 @@
+﻿namespace SAM.Core.Mollier
+{
+    //Limits
+    public static class Limit
+    {
+        public const double Density_Min = 0.45;
+        public const double Density_Max = 1.41;
+
+        public const double Enthalpy_Min = -20; //TODO: [MACIEK] Implement Enthalpy, use SI units [J/kg]
+        public const double Enthalpy_Max = 140; //TODO: [MACIEK] Implement Enthalpy, use SI units [J/kg]
+
+        public const double SpecificVolume_Min = 0.65;
+        public const double SpecificVolume_Max = 1.92;
+
+        public const double WetBulbTemperature_Min = -10;
+        public const double WetBulbTemperature_Max = 30;
+
+        public const double DryBulbTemperature_Min = -20;
+        public const double DryBulbTemperature_Max = 50;
+
+        public const double HumidityRatio_Min = 0;//TODO: [MACIEK] Update Units Humidity Ratio [kg_waterVapor/kg_dryAir]
+        public const double HumidityRatio_Max = 55; //TODO: [MACIEK] Update Units Humidity Ratio [kg_waterVapor/kg_dryAir]
+    }
+}
