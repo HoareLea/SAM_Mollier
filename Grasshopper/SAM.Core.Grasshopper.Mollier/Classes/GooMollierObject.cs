d3945a0e8156003f8e976c0320278f06d0ae6e7d
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2023-08-28 11:02:31 +0200

    CreateMollierGroup Grasshopper Component Added

---
+++ Grasshopper/SAM.Core.Grasshopper.Mollier/Classes/GooMollierObject.cs
@@ -0,0 +1,76 @@
+﻿using Grasshopper.Kernel;
+using Grasshopper.Kernel.Types;
+using SAM.Core.Grasshopper.Mollier.Properties;
+using SAM.Core.Mollier;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace SAM.Core.Grasshopper.Mollier
+{
+    public class GooMollierObject : GooJSAMObject<IMollierObject>
+    {
+        public GooMollierObject()
+            : base()
+        {
+        }
+
+        public GooMollierObject(IMollierObject mollierObject)
+            : base(mollierObject)
+        {
+        }
+
+        public override IGH_Goo Duplicate()
+        {
+            return new GooMollierObject(Value);
+        }
+
+        public override bool CastFrom(object source)
+        {
+            return base.CastFrom(source);
+        }
+
+        public override bool CastTo<Y>(ref Y target)
+        {
+            return base.CastTo(ref target);
+        }
+    }
+
+    public class GooMollierObjectParam : GH_PersistentParam<GooMollierObject>
+    {
+        public override Guid ComponentGuid => new Guid("598315c1-c756-4bbb-b66e-c6640e56ff99");
+
+        protected override Bitmap Icon => Resources.SAM_Small;
+
+        public GooMollierObjectParam()
+            : base("MollierObject", "MollierObject", "SAM Core MollierObject", "Params", "SAM")
+        {
+        }
+
+        protected override GH_GetterResult Prompt_Plural(ref List<GooMollierObject> values)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override GH_GetterResult Prompt_Singular(ref GooMollierObject value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void AppendAdditionalMenuItems(ToolStripDropDown menu)
+        {
+            Menu_AppendItem(menu, "Save As...", Menu_SaveAs, VolatileData.AllData(true).Any());
+
+            //Menu_AppendSeparator(menu);
+
+            base.AppendAdditionalMenuItems(menu);
+        }
+
+        private void Menu_SaveAs(object sender, EventArgs e)
+        {
+            Grasshopper.Query.SaveAs(VolatileData);
+        }
+    }
+}
---
+++ Grasshopper/SAM.Core.Grasshopper.Mollier/Component/SAMMollierCreateMollierGroup.cs
@@ -0,0 +1,106 @@
+﻿using Grasshopper.Kernel;
+using SAM.Core.Grasshopper.Mollier.Properties;
+using System;
+using System.Collections.Generic;
+using SAM.Core.Mollier;
+
+namespace SAM.Core.Grasshopper.Mollier
+{
+    public class SAMMollierCreateMollierGroup : GH_SAMVariableOutputParameterComponent
+    {
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid => new Guid("f5c44450-5d9e-4f7c-8a65-c8be36da7cd2");
+
+        /// <summary>
+        /// The latest version of this component
+        /// </summary>
+        public override string LatestComponentVersion => "1.0.0";
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon => Resources.SAM_Mollier;
+
+        public override GH_Exposure Exposure => GH_Exposure.primary;
+
+        protected override GH_SAMParam[] Inputs
+        {
+            get
+            {
+                List<GH_SAMParam> result = new List<GH_SAMParam>();
+
+                result.Add(new GH_SAMParam(new GooMollierObjectParam() { Name = "_mollierObjects", NickName = "_mollierObjects", Description = "Mollier Objects", Access = GH_ParamAccess.list, Optional = false }, ParamVisibility.Binding));
+
+                global::Grasshopper.Kernel.Parameters.Param_String param_String = new global::Grasshopper.Kernel.Parameters.Param_String() { Name = "_name_", NickName = "_name_", Description = "Group Name", Access = GH_ParamAccess.item, Optional = true };
+                param_String.SetPersistentData("New Group");
+                result.Add(new GH_SAMParam(param_String, ParamVisibility.Voluntary));
+
+                return result.ToArray();
+            }
+        }
+
+        protected override GH_SAMParam[] Outputs
+        {
+            get
+            {
+
+                List<GH_SAMParam> result = new List<GH_SAMParam>();
+
+                result.Add(new GH_SAMParam(new GooMollierGroupParam() { Name = "mollierGroup", NickName = "mollierGroup", Description = "Mollier Group", Access = GH_ParamAccess.item }, ParamVisibility.Binding));
+                return result.ToArray();
+            }
+        }
+
+        /// <summary>
+        /// Updates PanelTypes for AdjacencyCluster
+        /// </summary>
+        public SAMMollierCreateMollierGroup()
+          : base("SAMMollier.CreateMollierGroup", "SAMMollier.CreateMollierGroup",
+              "Create Mollier Group",
+              "SAM", "Mollier")
+        {
+        }
+
+        protected override void SolveInstance(IGH_DataAccess dataAccess)
+        {
+            int index;
+
+            List<IMollierObject> mollierObjects = new List<IMollierObject>();
+            index = Params.IndexOfInputParam("_mollierObjects");
+            if (index == -1 || !dataAccess.GetDataList(index, mollierObjects))
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
+                return;
+            }
+
+            string name = "New Group";
+            index = Params.IndexOfInputParam("_name_");
+            if (index == -1 || !dataAccess.GetData(index, ref name))
+            {
+                name = "New Group";
+            }
+
+            MollierGroup mollierGroup = new MollierGroup(name);
+            foreach(IMollierObject mollierObject in mollierObjects)
+            {
+                IMollierGroupable mollierGroupable = mollierObject as IMollierGroupable;
+                if(mollierGroupable == null)
+                {
+                    continue;
+                }
+
+                mollierGroup.Add(mollierGroupable);
+            }
+
+
+            index = Params.IndexOfOutputParam("mollierGroup");
+            if (index != -1)
+            {
+                dataAccess.SetDataList(index, mollierGroup);
+            }
+        }
+
+    }
+}
