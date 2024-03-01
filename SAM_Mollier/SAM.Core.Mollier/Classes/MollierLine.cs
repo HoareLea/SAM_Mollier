092e14c4077a11d42a5a222c47f2c1b668ff0118
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2024-03-01 17:51:13 +0100

    MollierLine Implementation

---
+++ SAM_Mollier/SAM.Core.Mollier/Classes/MollierLine.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace SAM.Core.Mollier
+{
+    public abstract class MollierLine : MollierCurve
+    {
+        internal MollierLine(MollierPoint mollierPoint)
+            : base(mollierPoint)
+        {
+
+        }
+
+        public MollierLine(MollierLine mollierLine)
+            :base(mollierLine)
+        {
+
+        }
+
+        public virtual bool FromJObject(JObject jObject)
+        {
+            bool result = base.FromJObject(jObject);
+            if (!result)
+            {
+                return false;
+            }
+
+            return result;
+        }
+
+        public virtual JObject ToJObject()
+        {
+            JObject result = base.ToJObject();
+            if(result == null)
+            {
+                return result;
+            }
+
+            return result;
+        }
+    }
+}
